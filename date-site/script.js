b1e29407ecbf4dad3346f5bc021dad08d3b71f54
Yoan Yovkov <joan414132@gmail.com>
2023-07-30 18:48:41 +0300

    aaa

---
+++ date-site/script.js
@@ -0,0 +1,45 @@
+const arrowIcon = document.querySelector('.fas.fa-angle-down');
+const languagesDiv = document.getElementById('languages');
+
+arrowIcon.addEventListener('click', () => {
+  languagesDiv.classList.toggle('show');
+});
+
+const slider = document.querySelector('.slider');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const images = document.querySelectorAll('.slider img');
+
+let counter = 0;
+
+nextBtn.addEventListener('click', () => {
+  counter++;
+  if (counter >= images.length) {
+    counter = 0;
+  }
+  updateSlider();
+});
+
+prevBtn.addEventListener('click', () => {
+  counter--;
+  if (counter < 0) {
+    counter = images.length - 1;
+  }
+  updateSlider();
+});
+
+function updateSlider() {
+  slider.style.transform = `translateX(-${counter * 100}%)`;
+}
+
+function showNextPrev() {
+  prevBtn.style.opacity = 1;
+  nextBtn.style.opacity = 1;
+  if (counter === 0) {
+    prevBtn.style.opacity = 0.5;
+  } else if (counter === images.length - 1) {
+    nextBtn.style.opacity = 0.5;
+  }
+}
+
+showNextPrev();
